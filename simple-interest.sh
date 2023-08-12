c0e72e508d08392bf1acb9b1300b0f51fa6b503c
Ash <89018395+ash413@users.noreply.github.com>
2023-08-12 13:06:59 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,13 +4,14 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # ash413
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
    # Output:
    # simple interest = p*t*r
+   
    echo "Enter the principal:"
    read p
    echo "Enter rate of interest per year:"
